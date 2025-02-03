3910af0e556cd71dbb1052f617d6f5012656108e
suuny0321 <joosun7647@naver.com>
2025-02-03 09:37:53 +0900

    Revert "Feature/#15 reviewer-ranking"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
-export { default as MainPage } from "./main/page/MainPage";
-export { default as MyPage } from "./myPage/page/MyPage";
-export { default as SignUpPage } from "./signUp/page/SignUpPage";
-export { default as LoginPage } from "./loginPage/page/LoginPage";
-export { default as RankingPage } from "./ranking/page/RankingPage";
+export { default as MainPage } from './main/page/MainPage';
+export { default as MyPage } from './myPage/page/MyPage';
+export { default as SignUpPage } from './signUp/page/SignUpPage';
+export { default as LoginPage } from './loginPage/page/LoginPage';

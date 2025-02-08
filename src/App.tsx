ee4f9be2280745bc1231d43a714d32d6b38bc931
junghyun13 <ljh041314@gmail.com>
2025-02-08 10:39:08 +0900

    유저 id랑 연결안됨  DB 더추가 필요

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/layout/Header";
-import Sidebar from "./components/layout/Sidebar";
+//import Sidebar from "./components/layout/Sidebar";
 import MapArea from "./components/map/MapArea";
 
 import CommunityPage from "./components/pages/community/CommunityPage";
 import WriteForm from "./components/pages/community/WriteForm";
 import PostDetail from "./components/pages/community/PostDetail";
 import { store } from "./utils/store/Store";
+import RestaurantDetail from "./components/pages/recommendedRestaurants/RestaurantDetail";
 import { Provider } from "react-redux";
 import React from "react";
 
@@ -43,7 +44,9 @@ function App() {
                                 /> */}
                 <Route path="/community" element={<CommunityPage />} />
                 <Route path="/community/writeForm" element={<WriteForm />} />
+                <Route path="/community/:id" element={<PostDetail />} />
                 <Route path="/reviewers" element={<PopularReviewersPage />} />
+                <Route path="/restaurant/:id" element={<RestaurantDetail />} />
                 <Route
                   path="/auth/naver/callback"
                   element={<AuthNaverCallback />}

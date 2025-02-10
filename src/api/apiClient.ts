e93f24829c1e58c09db7cc7089665fa983b310b4
seonghyeok <chosh2001@naver.com>
2025-02-10 13:58:54 +0900

    fix2

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || 'http://52.79.232.45:8090',
+    baseURL: import.meta.env.VITE_API_BASE_URL || 'https://52.79.232.45:8090',
     headers: {
         'Content-Type': 'application/json',
     },

afebd16f1d0b54878a1e55fcf9156befb22c5201
seonghyeok <chosh2001@naver.com>
2025-02-11 11:33:19 +0900

    fix: env

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     server: {
+        host: 'www.commars.whqtker.site',
         https: true,
         headers: {
             'Cross-Origin-Opener-Policy': 'unsafe-none',

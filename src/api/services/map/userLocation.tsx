fca061650b0b985f68dbdcbf75c50f261278e632
shjk0531 <shinjk0531@gmail.com>
2025-02-12 01:48:54 +0900

    fix userLocationService api

--- src/api/services/map/userLocation.tsx
+++ src/api/services/map/userLocation.tsx
@@ -2,7 +2,7 @@ import apiClient from "../../apiClient";
 
 export const userLocationService = {
   async getUserLocation() {
-    const response = await apiClient.get("/api/v1/user/location");
+    const response = await apiClient.get("/api/v1/location/current");
     console.log("유저 위치 정보 응답: ", response.data);
     return response.data;
   },

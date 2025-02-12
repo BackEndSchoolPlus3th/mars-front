ac05d848b91dd21691e40a2c6550c49f38c798e3
seonghyeok <chosh2001@naver.com>
2025-02-12 10:42:11 +0900

    fix: 로컬 실행 가능, 다른 기능X

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,26 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  if (mode === 'development') {
+    return {
+      plugins: [react(), tailwindcss()],
+      server: {
+        host: true,
+        port: 5173
+      }
+    }
+  }
+  
+  return {
     plugins: [react(), tailwindcss()],
     server: {
-        host: 'www.commars.whqtker.site',
-        https: true,
-        headers: {
-            'Cross-Origin-Opener-Policy': 'unsafe-none',
-            'Cross-Origin-Embedder-Policy': 'unsafe-none',
-        },
-    },
+      host: 'www.commars.whqtker.site',
+      https: true,
+      headers: {
+        'Cross-Origin-Opener-Policy': 'unsafe-none',
+        'Cross-Origin-Embedder-Policy': 'unsafe-none',
+      }
+    }
+  }
 });
